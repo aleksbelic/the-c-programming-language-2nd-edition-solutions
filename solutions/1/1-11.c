96c469ee20362ffca42cb2a00bdf63be561da08d
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2025-06-25 01:23:34 +0200

    Solutions 1-11, 1-12.

--- solutions/1/1-11.c
+++ solutions/1/1-11.c
@@ -2,3 +2,12 @@
 Exercise 1-11. How would you test the word count program? What kinds of input are most
 likely to uncover bugs if there are any?
 */
+
+/*
+Check with various inputs:
+  - empty input
+  - single word
+  - multiple words separated by spaces, tabs, or newlines
+  - only spaces, tabs, and newlines
+  - special characters
+*/
--- solutions/1/1-12.c
+++ solutions/1/1-12.c
@@ -1,3 +1,38 @@
 /*
 Exercise 1-12. Write a program that prints its input one word per line.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int c;
+    int inWord = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\t' || c == '\n')
+        {
+            if (inWord)
+            {
+                inWord = 0;
+                putchar('\n');
+            }
+        }
+        else
+        {
+            inWord = 1;
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
+
+/*
+   This         is a test
+This
+is
+a
+test
+*/
